35c6e292955b218314d5533e0703ec96a05e0031
Alex <bugrimov_a@bk.ru>
2021-05-14 09:47:59 +0300

    Added repositories

---
+++ zoo-backend/src/main/java/dev/bug/zoobackend/repository/ImageRepository.java
@@ -0,0 +1,15 @@
+package dev.bug.zoobackend.repository;
+
+import dev.bug.zoobackend.entity.ImageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+
+    Optional<ImageModel> findByUserId(Long userId);
+
+    Optional<ImageModel> findByPostId(Long postId);
+}
