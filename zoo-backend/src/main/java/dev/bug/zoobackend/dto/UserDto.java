bed60f702bb2c887c7982946b9e61e26ea5913b7
Alex <bugrimov_a@bk.ru>
2021-05-19 23:44:31 +0300

    Added UserDto

---
+++ zoo-backend/src/main/java/dev/bug/zoobackend/dto/UserDto.java
@@ -0,0 +1,18 @@
+package dev.bug.zoobackend.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class UserDto {
+
+    private Long id;
+    @NotEmpty
+    private String firstname;
+    @NotEmpty
+    private String lastname;
+    @NotEmpty
+    private String username;
+    private String bio;
+}
--- zoo-backend/src/main/java/dev/bug/zoobackend/service/UserService.java
+++ zoo-backend/src/main/java/dev/bug/zoobackend/service/UserService.java
@@ -1,13 +1,17 @@
 package dev.bug.zoobackend.service;
 
+import dev.bug.zoobackend.dto.UserDto;
 import dev.bug.zoobackend.entity.User;
 import dev.bug.zoobackend.entity.enums.RoleEnum;
 import dev.bug.zoobackend.exceptions.UserExistException;
 import dev.bug.zoobackend.payload.request.SignupRequest;
 import dev.bug.zoobackend.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Slf4j
 @Service
@@ -37,5 +41,23 @@ public class UserService {
             log.error("Error during registration. {}", ex.getMessage());
             throw new UserExistException("The user " + user.getUsername() + " already exist. Please check credentials");
         }
+    }
+
+    public User updateUser(UserDto userDto, Principal principal) {
+        var user = getUserByPrincipal(principal);
+        user.setName(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setBio(user.getBio());
+        return userRepository.save(user);
+    }
+
+    public User getCurrentUser(Principal principal) {
+        return getUserByPrincipal(principal);
+    }
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("USer not found with username " + username));
     }
 }
